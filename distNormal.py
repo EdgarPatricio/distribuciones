c865e4394504e961da2630855ef28bf9172bc050
EdgarSanchez <epsm96@gmail.com>
2019-07-02 08:00:19 -0500

    first commit

--- distNormal.py
+++ distNormal.py
@@ -2,19 +2,55 @@ import matplotlib.pyplot as plt
 import numpy as np 
 from scipy import stats 
 import seaborn as sns
-import easygui as eg 
+import easygui as eg
+import statistics as stat #libreria para calculos estadisticos
+
+
+mu = eg.integerbox(msg='Entrada de la media:',
+                    title='Distribución Normal',
+                    default='',
+                    lowerbound=0,
+                    upperbound=99,
+                    image=None)
+
+sigma1 = eg.enterbox(msg='Entrada de la desviación estandar:',
+                    title='Distribución Normal',
+                    default='',
+                    image=None)
+
+print(sigma1)
+
+sigma = float(sigma1)  
 
 # Graficando Normal
-mu, sigma = 0, 0.2 # media y desvio estandar
+# mu, sigma = 0, 0.2 # media y desvio estandar
 normal = stats.norm(mu, sigma)
 x = np.linspace(normal.ppf(0.01),
                 normal.ppf(0.99), 100)
+print(x) # en el eje x de la gráfica
+
+# Calculo de valores estadisticos media mediana, varianzay desviación estándar
+
+media = str(stat.mean(x))
+mediana = str(stat.median(x))
+varianza = stat.pvariance(x)
+desv = str(stat.pstdev(x))
+a = "Media",media,"\nMediana: ",mediana, "\nVarianza Poblacional: ",varianza,"\nDesviacion Estandar: ",desv
+
+
+
 fp = normal.pdf(x) # Función de Probabilidad
-plt.plot(x, fp)
+print(fp) # en el eje y de la gráfica
+plt.plot(x, fp) # grafica en funcionde x e y
+# leyendas para la gráfica
 plt.title('Distribución Normal')
-plt.ylabel('probabilidad')0.89376,0.89376, 0.97957, 0.07095, 0.07849, 0.080.89376,0.89376, 0.97957, 0.07095, 0.07849, 0.08
-plt.xlabel('valores')
+plt.ylabel('Probabilidad')
+plt.xlabel('Valores')
 plt.show()
+eg.msgbox(msg = a,
+          title='Control: msgbox', 
+          ok_button='Continuar',
+          image=None)
 
 # histograma
 aleatorios = normal.rvs(1000) # genera aleatorios
